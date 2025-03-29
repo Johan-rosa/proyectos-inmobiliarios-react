c60c96e65c7d1f9379d94ac4632d9b4ad6706568
Johan Rosa <johan.rosaperez@gmail.com>
2025-03-29 08:23:59 -0400

    feat: add sidebar layout

---
+++ components/sidebar.tsx
@@ -0,0 +1,52 @@
+import { WalletCards, Map } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+  {
+    title: "Planes de pago",
+    url: "#",
+    icon: WalletCards,
+  },
+  {
+    title: "Mapa de proyectos",
+    url: "#",
+    icon: Map,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Proyectos Inmobiliarios</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
