d6b07e57df7e6a3e0ed95a267e6c8d970254be3d
Johan Rosa <johan.rosaperez@gmail.com>
2025-04-06 10:11:24 -0400

    refactor: remove usePaymenPlan hook, we're not using the realtime database anymore

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,12 @@
 'use client'
 
-import React, { useEffect, useState } from "react";
 import { PageHeader } from "@/components/app-header";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { usePaymentPlans } from '@/hooks/use-payment-plans';
 import PaymentPlansTable from "@/app/payments-table"
-import type { PaymentPlan } from '@/types';
 
 export default function Home() {
-
-  const { plans, loading, error } = usePaymentPlans();
-  const [plansData, setPlansData] = useState<PaymentPlan[]>([]);
-
-  useEffect(() => {
-    if (!loading && !error) {
-      setPlansData(plans);
-    }
-  }, [plans, loading, error]);
-
-  console.log(plansData);
-
   return (
     <main>
       <PageHeader>
